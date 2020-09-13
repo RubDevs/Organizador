415f3d39626f95295f752638b2593fac35bac8e7
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-09-12 19:25:50 -0500

    Service object to manage states of task created

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,14 @@
 class Task
   include Mongoid::Document
   include Mongoid::Timestamps
+  include AASM
 
   field :name, type: String
   field :description, type: String
   field :due_date, type: Date
   field :role, type: Integer
+  field :status, type: String
+  field :transitions, type: Array, default: []
 
   belongs_to :category
   belongs_to :owner, class_name: 'User'
@@ -20,6 +23,32 @@ class Task
   validate :due_date_validity
 
   accepts_nested_attributes_for :participating_users, allow_destroy: true
+
+  aasm column: :status do
+    state :pending, initial: true
+    state :in_process, :finished
+
+    after_all_transitions :audit_status_change
+
+    event :start do
+      transitions from: :pending, to: :in_process
+    end
+
+    event :finish do
+      transitions from: :in_process, to: :finished
+    end
+  end
+
+  def audit_status_change
+    set transitions: transitions.push(
+      {
+        from_state: aasm.from_state,
+        to_state: aasm.to_state,
+        current_event: aasm.current_event,
+        timestamp: Time.zone.now
+      }
+    )
+  end
 
   def participants
     participating_users.includes(:user).map(&:user)
